e7561b745e2e80d4bc7905df77446c17127436ad
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-04-28 12:54:18 -0300

    style: lines logo

--- src/ui/lines-logo/view.tsx
+++ src/ui/lines-logo/view.tsx
@@ -1,20 +1,18 @@
 import Link from "next/link";
-import { IoIosBus } from "react-icons/io";
 import { DefaultProps } from "@/types/default-props";
 
 export const LinesLogoView = ({ mobile }: DefaultProps) => {
   return (
     <section>
-      <Link href="/" className="flex gap-1 items-end" title="_lines">
+      <Link href="/" className="flex items-end" title="_lines">
         <span
           className={`
-            ${mobile ? "text-3xl" : "text-4xl"} 
-            lines-text font-bold text-green-500
+            ${mobile ? "text-3xl" : "text-3xl"} 
+            lines-text font-extrabold
           `}
         >
-          _lines
+          --<span className="text-green-500">LINES</span>--
         </span>
-        <IoIosBus className={mobile ? "w-9 h-9" : "w-10 h-10"} />
       </Link>
     </section>
   );
